e61844c5697657c3015f1f607d5056d206c2b04d
André Singh <quatinhe@outlook.com>
2025-05-08 19:53:25 +0100

    new asset

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -6,7 +6,7 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
 
     [Tooltip("How quickly the camera catches up to the target")]
-    public float smoothTime = 0.2f;
+    public float smoothTime = 0.0f;
 
     [Tooltip("Offset from the target's position")]
     public Vector3 offset = new Vector3(0f, 1f, -5f);
